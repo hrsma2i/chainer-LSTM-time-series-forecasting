0dd44a2e024a14be6916d631ab1da607c5fe8cca
hrsma2i <hrs.ma2i@gmail.com>
2017-11-02 10:39:59 +0900

    Add tune_seq

--- verify.py
+++ verify.py
@@ -47,7 +47,6 @@ def tune_prc(root, series, n_sample=100, n_epoch=300):
     routin(name_prc, prcsr)
     
     sclrs ={
-        'minmax':MinMaxScaler((-1,1)),
         'minmax+':MinMaxScaler((0,1)),
         'standard':StandardScaler(),
         'not_scale':None,
@@ -63,22 +62,48 @@ def tune_prc(root, series, n_sample=100, n_epoch=300):
 
 # In[ ]:
 
-data_root = 'data'
-name_seq = 'airline'
-
-for name_seq in f.readlines:
-
-path_csv = os.path.join(data_root, 
-                        '{}_train.csv'.format(name_seq))
-
-series = pd.read_csv(path_csv, header=None).values
-
-root = 'result/test_prc_loop'
-if not os.path.exists(root):
-    os.mkdir(root)
+def tune_seq(data_root, path_sequences, root, 
+            n_sample=100, n_epoch=300):
+    """
+    # Param
+    - data_root (str): where seq csv-data are
+    - path_sequences (str): where sequences-list are
+    - root (srt): root dir where results will be dumped
+    """
+    seqs = [ seq.rstrip()
+            for seq in open(path_sequences, 'r').readlines()]
     
-tune_prc(root=path_seq, series=series, 
-         n_sample=2, n_epoch=2)
+    for name_seq in seqs:
+        print(name_seq)
+        path_csv = os.path.join(data_root, 
+                                '{}_train.csv'.format(name_seq))
+
+        series = pd.read_csv(path_csv, header=None).values
+
+        path_seq = os.path.join(root, name_seq)
+        if not os.path.exists(path_seq):
+            os.mkdir(path_seq)
+
+        tune_prc(root=path_seq, series=series, 
+                 n_sample=n_sample, n_epoch=n_epoch)
+
+
+# In[ ]:
+
+if __name__=="__main__":
+    # for test
+    #n_sample = 2
+    #n_epoch  = 2
+    n_sample = 100
+    n_epoch  = 300
+    data_root = 'data'
+    path_sequences = os.path.join(data_root, 'sequences_verify')
+    root = 'result'
+    if not os.path.exists(root):
+        os.mkdir(root)
+    tune_seq(data_root=data_root,
+            path_sequences=path_sequences, root=root,
+            n_sample=n_sample, n_epoch=n_epoch)
 
 
 # In[ ]:
